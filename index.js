b14e6eb3241b6dd9f97a6063c8db896b02d15d1a
NickSchirripa <108542730+NickSchirripa@users.noreply.github.com>
2024-04-29 15:56:09 -0400

    Render replies
    rendered out the replies to the HTML.

--- index.js
+++ index.js
@@ -62,14 +62,23 @@ function getFeedHtml(){
             retweetIconClass = 'retweeted'
         }
         
-/*
-Challenge:
-1. Use an if statement to set the value of 
-   'retweetIconClass' to the string 
-   'retweeted' if the tweet has been retweeted. 
-2. In the retweet icon tag, add 'retweetIconClass' 
-   to the list of classes.
-*/
+        let repliesHtml = ''
+
+        if (tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml += `
+                <div class="tweet-reply">
+                <div class="tweet-inner">
+                    <img src="${reply.profilePic}" class="profile-pic">
+                        <div>
+                            <p class="handle">${reply.handle}</p>
+                            <p class="tweet-text">${reply.tweetText}</p>
+                        </div>
+                    </div>
+            </div>
+            `
+            })
+        }
           
         feedHtml += `
 <div class="tweet">
@@ -99,6 +108,9 @@ Challenge:
                 </span>
             </div>   
         </div>            
+    </div>
+    <div id="replies-${tweet.uuid}">
+    ${repliesHtml}
     </div>
 </div>
 `
