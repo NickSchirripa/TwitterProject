1d3f72bfae5d765066a7f1e300884cc375e2a892
NickSchirripa <108542730+NickSchirripa@users.noreply.github.com>
2024-04-29 17:14:47 -0400

    Reply Toggle
    made an event listener that targets the "reply" button, and a function() that is launched when it is clicked. This function toggles the class "hidden". This class will unhide and hide the replies.

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ document.addEventListener('click', function(e){
     else if(e.target.dataset.retweet){
         handleRetweetClick(e.target.dataset.retweet)
     }
+    else if(e.target.dataset.reply){
+        handleReplyClick(e.target.dataset.reply)
+    }
 })
 
 function handleLikeClick(tweetId){ 
@@ -43,6 +46,10 @@ function handleRetweetClick(tweetId){
     }
     targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
     render() 
+}
+
+function handleReplyClick(replyId){
+    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
 }
 
 function getFeedHtml(){
@@ -109,7 +116,7 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
-    <div id="replies-${tweet.uuid}">
+    <div class="hidden" id="replies-${tweet.uuid}">
     ${repliesHtml}
     </div>
 </div>
